5e44e712ef5e26906d62542aa328a6fd112114ce
cis101 <cis101@secs.iub.edu.bd>
2019-05-27 09:33:21 +0600

    Task-3

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 /* JavaScript code here */
 console.log("External Javascript")
+
+// Task 2: Array Sum
+
+function sum(arr){
+    var sum=0;
+    arr.forEach(function(item, index){
+        sum=sum+item;
+    })
+    return sum;
+}
+console.log(sum([1,2,3,4]))
+
+// Task 3: Email Validation regex
+var validateEmail= function validateEmail(email){
+   return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+}
+var validatingEmail= validateEmail('hjhscsdjcd')
+console.log(validatingEmail)
+console.log(validateEmail('djjeed@task.com'))
